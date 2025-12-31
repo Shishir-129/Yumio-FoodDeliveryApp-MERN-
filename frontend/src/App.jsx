16ec3f5ba21b6041bc8469bc130bbf49559ac181
Shishir Neupane <shishirapk@gmail.com>
2025-12-31 20:43:37 +0545

    changes in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 import Footer from "./components/Footer/Footer";
+import LoginPopup from "./components/LoginPopup/LoginPopup";
 
 const App = () => {
+
+  const [showLogin,setShowLogin] = useState(false);
+
   return (
     <>
+    {showLogin?<LoginPopup />:<></>}
       <div className="app">
-        <Navbar />
+        <Navbar setShowLogin={setShowLogin}/>
         <Routes>
-          <Route path="/" element={<Home />} />{" "}
+          <Route path="/" element={<Home />} />
           {/* When we type '/' it will goto Home page*/}
-          <Route path="/cart" element={<Cart />} />{" "}
+          <Route path="/cart" element={<Cart />} />
           {/* When we type '/cart' it will goto Cart page*/}
           <Route path="/order" element={<PlaceOrder />} />
         </Routes>
