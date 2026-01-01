fe922b9e5a72bb99989dc021c361b3ce3c2634e5
Shishir Neupane <shishirapk@gmail.com>
2026-01-01 22:09:35 +0545

    loginpopup changes

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './LoginPopup.css'
+import { assets } from '../../assets/assets'
 
-const LoginPopup = () => {
+const LoginPopup = ({setShowLogin}) => {
+
+  const [currState,setCurrState] = useState("Login")
+
   return (
     <div className='login-popup'>
-      Login
+      <form className="login-popup-container">
+        <div className="login-popup-title">
+          <h2>{currState}</h2>
+          <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
+        </div>
+        <div className="login-popup-inputs">
+          {/* If current state is Login then don't show name input field */}
+          {currState==="Login"?<></>:<input type="text" placeholder='Your name' required />}
+          <input type="email" placeholder="Your email" required />
+          <input type="password" placeholder="Password" required />
+        </div>
+        <button>{currState=="Sign Up"?"Create Account":"Login"}</button>
+      </form>
     </div>
   )
 }
