119f90cbf644ecc9a734091406171c1daa82314f
Shishir Neupane <shishirapk@gmail.com>
2026-02-03 22:53:30 +0545

    verification changes #1

--- frontend/src/pages/Verify/Verify.jsx
+++ frontend/src/pages/Verify/Verify.jsx
@@ -1,6 +1,7 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
+import axios from 'axios'
 import './Verify.css'
-import { useSearchParams } from 'react-router-dom'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 import { StoreContext } from '../../context/StoreContext';
 
 const Verify = () => {
@@ -9,8 +10,20 @@ const Verify = () => {
     const success = searchParams.get("success");
     const orderId = searchParams.get("orderId");
     const {url}   = useContext(StoreContext);
+    const navigate = useNavigate();
     
+    const verifyPayment = async () => {
+      const response = await axios.post(url + "/api/order/verify", {success, orderId});
+      if(response.data.success) {
+        navigate("/myorders");
+      } else {
+        navigate("/");
+      }
+    }
 
+    useEffect( () => {
+      verifyPayment();
+    },[])
     
   return (
     <div className='verify'>
