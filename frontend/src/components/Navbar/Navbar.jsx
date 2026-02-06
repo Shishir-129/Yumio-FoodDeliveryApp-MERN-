abeb5ba32416ff107359ff29344cd7c42c8d89b7
Shishir Neupane <shishirapk@gmail.com>
2026-02-06 22:24:37 +0545

    order status logic

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -28,8 +28,8 @@ const Navbar = ({setShowLogin}) => {
         <a href='#footer' onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>Contact-us</a>
       </ul>
       <div className="navbar-right">
-        <img src={assets.search_icon} alt="" />
-        <div className="navbar-search-icon">
+        <img className="search-icon" src={assets.search_icon} alt="" />
+        <div className="navbar-basket-icon">
             {/* Link is used to navigate to cart page when we click on the basket icon */}
             <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link> 
             <div className={getTotalCartAmount()===0?"":"dot"}></div>
